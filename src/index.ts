9fc350184f540744e6a0b9c58b9d660de637e9a7
mohak <c.mohak2004@gmail.com>
2025-01-26 00:03:24 +0530

    API update -min

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import  express  from "express";
+import router from "./routes/AuthRoutes";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
+app.use("api/v1", router);
+
+app.listen(3000);
+console.log("server running on port 3000");
 
